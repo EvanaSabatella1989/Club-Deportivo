0ed2673a4526a6c23e0f1dfa49d7d2e1eb45ce5f
Melisa <melisasteinaker@gmail.com>
2024-05-22 17:06:23 -0300

    Pantallas Meli actualizadas

---
+++ Club/app/src/main/java/com/example/club/ElegirActividadNoSocioActivity.kt
@@ -0,0 +1,17 @@
+package com.example.club
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ElegirActividadNoSocioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_elegir_actividad_no_socio)
+
+    }
+
+}
