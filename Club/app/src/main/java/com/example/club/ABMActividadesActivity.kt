fd0fd6fc8807ca7003a88442e52bc990c0b48c40
sergioacos <arqsergioacosta@gmail.com>
2024-05-24 00:11:15 -0300

    Pantalla actividades

--- Club/app/src/main/java/com/example/club/ABMActividadesActivity.kt
+++ Club/app/src/main/java/com/example/club/ABMActividadesActivity.kt
@@ -11,10 +11,10 @@ class ABMActividadesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_abmactividades)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
     }
 }
