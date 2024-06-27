123aadcf81b88357c7f11c22c69526c0958b1c70
ana-b-orellana <anaborellana1184@gmail.com>
2024-06-27 20:32:38 -0300

    tercer commit

--- Club/app/src/main/java/com/example/club/DatabaseHelper.kt
+++ Club/app/src/main/java/com/example/club/DatabaseHelper.kt
@@ -8,29 +8,29 @@ import com.example.club.models.Actividad
 class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION)  {
     companion object {
         private const val DATABASE_NAME = "ClubDatabase.db"
-        private const val DATABASE_VERSION = 1
+        private const val DATABASE_VERSION = 3
         private const val TABLE_USERS = "User"
         private const val COLUMN_ID = "id"
         private const val COLUMN_USERNAME = "username"
         private const val COLUMN_PASSWORD = "password"
 
-        private const val CREATE_PERSONA_TABLE = "CREATE TABLE persona " + "(idPersona INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, apellido TEXT, fechaNac DATE, dni INTEGER, aptoFisico TINYINT)"
-        private const val CREATE_SOCIO_TABLE = "CREATE TABLE socio " + "(idSocio INTEGER PRIMARY KEY AUTOINCREMENT, idPersona INTEGER, FOREIGN KEY (idPersona) REFERENCES persona(idPersona))"
-        private const val CREATE_NOSOCIO_TABLE = "CREATE TABLE nosocio " + "(idNoSocio INTEGER PRIMARY KEY AUTOINCREMENT, idPersona INTEGER, FOREIGN KEY (idPersona) REFERENCES persona(idPersona))"
-        private const val CREATE_CUOTA_TABLE = "CREATE TABLE cuota " + "(idCuota INTEGER PRIMARY KEY AUTOINCREMENT, idSocio INTEGER, monto FLOAT, fechaVencimiento DATE, periodo TEXT, medioPago TEXT, fechaEmision DATE, FOREIGN KEY (idSocio) REFERENCES socio(idSocio))"
+        private const val CREATE_PERSONA_TABLE = "CREATE TABLE persona " + "(idPersona INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, apellido TEXT, fechaNac DATE, dni INTEGER, isSocio TINYINT, aptoFisico TINYINT)"
+        //private const val CREATE_SOCIO_TABLE = "CREATE TABLE socio " + "(idSocio INTEGER PRIMARY KEY AUTOINCREMENT, idPersona INTEGER, FOREIGN KEY (idPersona) REFERENCES persona(idPersona))"
+        //private const val CREATE_NOSOCIO_TABLE = "CREATE TABLE nosocio " + "(idNoSocio INTEGER PRIMARY KEY AUTOINCREMENT, idPersona INTEGER, FOREIGN KEY (idPersona) REFERENCES persona(idPersona))"
+        private const val CREATE_CUOTA_TABLE = "CREATE TABLE cuota " + "(idCuota INTEGER PRIMARY KEY AUTOINCREMENT, idSocio INTEGER, monto FLOAT, fechaVencimiento DATE, periodo TEXT, medioPago TEXT, fechaEmision DATE, FOREIGN KEY (idPersona) REFERENCES persona(idPersona))"
         private const val CREATE_ACTIVIDAD_TABLE = "CREATE TABLE actividad " + "(idActividad INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, costo FLOAT)"
-        private const val CREATE_NOSOCIOACTIVIDAD_TABLE = "CREATE TABLE nosocioactividad " + "(idNoSocio INTEGER, idActividad INTEGER, PRIMARY KEY (idNoSocio, idActividad), FOREIGN KEY (idNoSocio) REFERENCES nosocio(idNoSocio), FOREIGN KEY (idActividad) REFERENCES actividad(idActividad))"
+        private const val CREATE_PERSONAACTIVIDAD_TABLE = "CREATE TABLE personaactividad " + "(idPersona INTEGER, idActividad INTEGER, PRIMARY KEY (idPersona, idActividad), FOREIGN KEY (idPersona) REFERENCES persona(idPersona), FOREIGN KEY (idActividad) REFERENCES actividad(idActividad))"
     }
 
     override fun onCreate(db: SQLiteDatabase) {
         val createUsersTable = "CREATE TABLE $TABLE_USERS ($COLUMN_ID INTEGER PRIMARY KEY, $COLUMN_USERNAME TEXT, $COLUMN_PASSWORD TEXT)"
         db.execSQL(createUsersTable)
         db.execSQL(CREATE_PERSONA_TABLE)
-        db.execSQL(CREATE_SOCIO_TABLE)
-        db.execSQL(CREATE_NOSOCIO_TABLE)
+        //db.execSQL(CREATE_SOCIO_TABLE)
+        //db.execSQL(CREATE_NOSOCIO_TABLE)
         db.execSQL(CREATE_CUOTA_TABLE)
         db.execSQL(CREATE_ACTIVIDAD_TABLE)
-        db.execSQL(CREATE_NOSOCIOACTIVIDAD_TABLE)
+        db.execSQL(CREATE_PERSONAACTIVIDAD_TABLE)
         //carga del usuario:
         val adminValues = ContentValues().apply {
             put(COLUMN_USERNAME, "clubdeportivo")
@@ -69,11 +69,11 @@ class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME,
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         db.execSQL("DROP TABLE IF EXISTS $TABLE_USERS")
         db.execSQL("DROP TABLE IF EXISTS persona")
-        db.execSQL("DROP TABLE IF EXISTS socio")
-        db.execSQL("DROP TABLE IF EXISTS nosocio")
+        //db.execSQL("DROP TABLE IF EXISTS socio")
+        //db.execSQL("DROP TABLE IF EXISTS nosocio")
         db.execSQL("DROP TABLE IF EXISTS cuota")
         db.execSQL("DROP TABLE IF EXISTS actividad")
-        db.execSQL("DROP TABLE IF EXISTS nosocioactividad")
+        db.execSQL("DROP TABLE IF EXISTS personaactividad")
         onCreate(db)
 
     }
